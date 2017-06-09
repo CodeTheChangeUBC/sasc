9fffd4fccaf38ad588ec1ec1b006deb67eeece65
Jonathan Funk <jonfunk88@hotmail.com>
2017-06-09 14:44:28 -0700

    server now returns index.html

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client', 'src')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'src', 'index.html'));
+});
 
 app.use('/', index);
 app.use('/users', users);
