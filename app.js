f0e78c36d9241e8793a0efac3c0ebb9bc4c0f333
Jonathan Funk <jonfunk88@hotmail.com>
2017-06-10 11:42:21 -0700

    fixed repeating a user connected message

--- app.js
+++ app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 
 app.use(express.static(path.resolve(__dirname, 'client', 'build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.use('*', function(req, res) {  
+ res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); 
 });
 
 app.use('/', index);
