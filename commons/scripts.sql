22644f22fa7c5d695937ab22e0ea6759864ceb83
Stephanie Chan <stephaniechan219@gmail.com>
2017-08-02 18:41:22 -0700

    scripts primary key fix

--- commons/scripts.sql
+++ commons/scripts.sql
@@ -11,40 +11,39 @@ CREATE TABLE twilio (
 );
 
 CREATE TABLE user (
-	ID int NOT NULL AUTO_INCREMENT, 
+	ID int NOT NULL AUTO_INCREMENT,
 	age int,
-	gender VARCHAR(24), 
-	phoneNumber VARCHAR(15) NOT NULL , 
-	password VARCHAR(30) NOT NULL, 
+	gender VARCHAR(24),
+	phoneNumber VARCHAR(15) NOT NULL ,
+	password VARCHAR(30) NOT NULL,
 	PRIMARY KEY (ID)
 );
 
 CREATE TABLE counsellor (
-	ID int NOT NULL AUTO_INCREMENT, 
-	firstName VARCHAR(30) NOT NULL, 
-	lastName VARCHAR(30) NOT NULL, 
-	email VARCHAR(256) NOT NULL, 
-	password VARCHAR(30) NOT NULL, 
+	ID int NOT NULL AUTO_INCREMENT,
+	firstName VARCHAR(30) NOT NULL,
+	lastName VARCHAR(30) NOT NULL,
+	email VARCHAR(256) NOT NULL,
+	password VARCHAR(30) NOT NULL,
 	PRIMARY KEY (ID)
 );
 
 CREATE TABLE session (
-	ID int NOT NULL AUTO_INCREMENT, 
-	beginTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-	endTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
-	counsellorID INT NOT NULL, 
-	userID INT, 
-	PRIMARY KEY (ID) REFERENCES counsellor(ID), 
+	ID int NOT NULL AUTO_INCREMENT,
+	beginTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	endTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	counsellorID INT NOT NULL,
+	userID INT,
+	PRIMARY KEY (ID),
 	FOREIGN KEY (userID) REFERENCES user(ID)
 );
 
 CREATE TABLE message (
-	sessionID int, 
-	messageTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL, 
-	counsellorID int, userID int, 
-	messageContent TEXT NOT NULL, 
-	PRIMARY KEY (sessionID), 
-	PRIMARY KEY (messageTime), 
-	FOREIGN KEY (counsellorID) REFERENCES counsellor(ID), 
+	sessionID int,
+	messageTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+	counsellorID int, userID int,
+	messageContent TEXT NOT NULL,
+	PRIMARY KEY (sessionID),
+	FOREIGN KEY (counsellorID) REFERENCES counsellor(ID),
 	FOREIGN KEY (userID) REFERENCES user(ID)
 );
