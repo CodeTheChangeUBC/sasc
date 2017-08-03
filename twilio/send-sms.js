fd3270f23a7c475a087fd529b310af7579be71a2
Stephanie Chan <stephaniechan219@gmail.com>
2017-08-02 18:37:24 -0700

    send-sms

--- twilio/send-sms.js
+++ twilio/send-sms.js
@@ -1,16 +1,47 @@
-// Twilio Credentials 
-var accountSid = 'AC049ba6d691d6a63674ebcd7a9caa433d'; 
-var authToken = '5789dbba242d79b808d45b68e6752354'; 
- 
-//require the Twilio module and create a REST client 
-var client = require('twilio')(accountSid, authToken); 
- 
-client.messages.create({ 
-    to: "+16042504126", 
-    from: "+16042657509", 
-    body: "Test message from twilio account", 
-}, function(err, message) { 
-    if (err) {
-    	console.log("DIDN'T WORK"); 
-    }
-});
+import { stephNumber, stephTwilioNumber, stephAuthToken, stephAccountSid } from './twilioCredentials';
+const twilios = require('./models/twilios').twilio;
+
+export default class SendSMS {
+	constructor() {
+		super(accountSid, authToken);
+		this.accountSid = stephAccountSid;
+		this.authToken = stephAuthToken;
+		//require the Twilio module and create a REST client
+		this.client = require('twilio')(accountSid, authToken);
+	}
+
+	/**
+		Sends message from Twilio number to SMS number.
+		Messages get stored to our database.
+	**/
+	sendMessageToSMS(obj, toNumber, fromNumber, body) {
+		obj.client.messages.create({
+		    to: stephNumber,
+		    from: stephTwilioNumber,
+		    body: body,
+		}, function(err, message) {
+		    if (err) {
+		    	console.log("DIDN'T WORK");
+		    }
+		    console.log(message.sid);
+				// TODO: Add message to database.
+				twilios.update("example@example.com", fromNumber, obj.accountSid, obj.authToken);
+		});
+	}
+
+	/**
+		Returns: a list of message objects for a specific contact
+	**/
+	listMessagesForContact(obj, contactNumber) {
+		var result = obj.client.messages.list(function(err, data) {
+			var messages = [];
+			data.messages.forEach(function(message) {
+				console.log(message.body);
+				if (message.to === contactNumber || message.from === contactNumber)
+					messages.push(message);
+			});
+			return messages;
+		});
+		return result;
+	}
+}
