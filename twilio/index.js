ffe0bc44716a0ff0b03ad52de577618d0573b77c
Stephanie Chan <stephaniechan219@gmail.com>
2017-08-23 18:00:08 -0700

    twilio backend reorganization again

--- twilio/index.js
+++ twilio/index.js
@@ -1,7 +1,7 @@
 // The purpose of this file is to export the functionality 
 // of the twilio model
 
-const twilio = require('models/twilios')
+const twilio = require('controllers/twilios')
 
 module.exports = {
 	twilio,
