9b20fe3ab84f4c45872eae75aa3b25fe321bf3ab
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-08 21:39:28 -0800

    try to make code consistent

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,20 +1,20 @@
-const jwt = require('jwt-simple');
-const Abstract = require('../models/abstract');
-const User = require('../models/user');
-const Counsellor = require('../models/counsellor');
-const config = require('../config');
+const jwt = require("jwt-simple");
+const Abstract = require("../models/abstract");
+const User = require("../models/user");
+const Counsellor = require("../models/counsellor");
+const config = require("../config");
 
 function tokenForUser(user, role) {
-    const timestamp = new Date().getTime();                         // in milliseconds
-    const expiry = (new Date().getTime() + 60 * 60 * 1000)/1000;    // An hour from now (in seconds)
-    return jwt.encode({ sub: user.ID, iat: timestamp, exp: expiry, role: role }, config.secret);
+    const timestamp = Date.now();                         // in milliseconds
+    const expiry = (Date.now() + 60 * 60 * 1000) / 1000;    // An hour from now (in seconds)
+    return jwt.encode({sub: user.ID, iat: timestamp, exp: expiry, role: role}, config.secret);
 }
 
-exports.signin = function(req, res) {
-    res.send({ token: tokenForUser(req.user, 'user') });
-}
+exports.signin = function (req, res) {
+    res.send({token: tokenForUser(req.user, "user")});
+};
 
-exports.signup = function(req, res, next) {
+exports.signup = function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
     const age = req.body.age;
@@ -23,23 +23,25 @@ exports.signup = function(req, res, next) {
     const email = req.body.email;
 
     if (!username || !password || !email) {
-        return res.status(422).send({ error: 'You must provide all of username, password and email.' });
+        return res.status(422).send({error: "You must provide all of username, password and email."});
     }
 
     // Welcome to callback hell :D
     // Check if user with this username already exists
-    User.lookupByUsername(username, function(err, existingUser) {
+    User.lookupByUsername(username, function (err, existingUser) {
 
-        if (err) { throw err; }
+        if (err) {
+            throw err;
+        }
 
         // If a user with the username already exists, return an error
         if (existingUser) {
-            return res.status(422).send({ error: 'Username is in use' });
+            return res.status(422).send({error: "Username is in use"});
         }
 
         var user = {
             username: username,
-            age: age, 
+            age: age,
             gender: gender,
             phoneNumber: phoneNumber,
             email: email,
@@ -50,49 +52,61 @@ exports.signup = function(req, res, next) {
             user = result;
             req.body.password = result.password;
 
-            User.create(user, res, function(err, result) {
-                if (err) { return res.status(422).send({ error: 'Cannot create user.'}); }
+            User.create(user, res, function (err) {
+                if (err) {
+                    return res.status(422).send({error: "Cannot create user."});
+                }
 
-                User.lookupIdByUsername(username, function(err, result) {
-                    if (err) { throw err; }
+                User.lookupIdByUsername(username, function (err, result) {
+                    if (err) {
+                        throw err;
+                    }
 
-                    if (!result) { return res.status(422).send({ error: 'Username does not exist.' }); }
+                    if (!result) {
+                        return res.status(422).send({error: "Username does not exist."});
+                    }
                     req.body.ID = result;
                     user.ID = result;
-                    
+
                     // Send token back to client
-                    res.json({ token: tokenForUser(user, 'user') });
+                    res.json({token: tokenForUser(user, "user")});
                 });
-            
+
             });
-        
+
         });
-    
+
     });
 
-}
+};
 
 // Authentication for counsellors
 
-exports.signinCounsellor = function(req, res) {
-    res.send({ token: tokenForUser(req.user, 'counsellor') });
-}
+exports.signinCounsellor = function (req, res) {
+    res.send({token: tokenForUser(req.user, "counsellor")});
+};
 
-exports.signupCounsellor = function(req, res, next) {
+exports.signupCounsellor = function (req, res) {
     const firstName = req.body.firstName;
     const lastName = req.body.lastName;
     const email = req.body.email;
     const password = req.body.password;
-    
-    if (!email || !password) { return res.status(422).send({ error: 'You must provide email and password.' }); }
+
+    if (!email || !password) {
+        return res.status(422).send({error: "You must provide email and password."});
+    }
 
     // Check if counsellor with this email already exists
-    Counsellor.lookupByEmail(email, function(err, existingCounsellor) {
+    Counsellor.lookupByEmail(email, function (err, existingCounsellor) {
 
-        if (err) { throw err; }
+        if (err) {
+            throw err;
+        }
 
         // If a counsellor with the email already exists, return an error
-        if (existingCounsellor) { return res.status(422).send({ error: 'Email is in use' }); }
+        if (existingCounsellor) {
+            return res.status(422).send({error: "Email is in use"});
+        }
 
         var counsellor = {
             firstName: firstName,
@@ -105,25 +119,31 @@ exports.signupCounsellor = function(req, res, next) {
             counsellor = result;
             req.body.password = result.password;
 
-            Counsellor.create(counsellor, res, function(err, result) {
-                if (err) { return res.status(422).send({ error: 'Cannot create counsellor.'}); }
+            Counsellor.create(counsellor, res, function (err) {
+                if (err) {
+                    return res.status(422).send({error: "Cannot create counsellor."});
+                }
 
-                Counsellor.lookupIdByEmail(email, function(err, result) {
-                    if (err) { throw err; }
+                Counsellor.lookupIdByEmail(email, function (err, result) {
+                    if (err) {
+                        throw err;
+                    }
 
-                    if (!result) { return res.status(422).send({ error: 'Email does not exist.' }); }
+                    if (!result) {
+                        return res.status(422).send({error: "Email does not exist."});
+                    }
 
                     req.body.ID = result;
                     counsellor.ID = result;
-                    
+
                     // Send token back to client
-                    res.json({ token: tokenForUser(counsellor, 'counsellor') });
+                    res.json({token: tokenForUser(counsellor, "counsellor")});
                 });
-            
+
             });
-        
+
         });
-    
+
     });
 
-}
+};
