dd7c49c0168b8d73cff98d07a13898d44c4fb86e
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-13 17:11:56 -0800

    remove AuthHeader from imports

--- client/src/Containers/App/index.js
+++ client/src/Containers/App/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './styles.css';
 import Header from './../Header';
-import AuthHeader from './../AuthHeader';
 import Main from './../Main';
 
 class App extends Component {
