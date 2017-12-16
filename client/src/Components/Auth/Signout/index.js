6a6200f1f73a8f6031f20780678a5cf803d01e8b
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-15 17:07:39 -0800

    signout message

--- client/src/Components/Auth/Signout/index.js
+++ client/src/Components/Auth/Signout/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import * as actions from '../../../Redux/Actions/authActions';
 import PropTypes from 'prop-types';
 
@@ -9,7 +10,9 @@ class Signout extends Component {
     }
     
     render() {
-        return <div>Bye!</div>;
+        return (
+            <div>Bye! Click <Link className="nav-link" to="/">here</Link> to go back to the homepage.</div>
+        );
     }
 }
 
