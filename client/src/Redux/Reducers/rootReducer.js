6e76c0cb8662bc54d7cf09363ea354ab58f1015a
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-15 19:40:37 -0800

    remove redux-form import from rootReducer

--- client/src/Redux/Reducers/rootReducer.js
+++ client/src/Redux/Reducers/rootReducer.js
@@ -4,14 +4,12 @@ import rooms from './roomsReducer';
 import activeRoom from './activeRoomReducer';
 import signInReducer from './signInReducer';
 import authReducer from './authReducer';
-import { reducer as form } from 'redux-form';
 
 const rootReducer = combineReducers({
   messages,
   rooms,
   activeRoom,
   signInReducer,
-  form,
   auth: authReducer
 });
 
