98a23bbd197d26f547412e54f68fa7449aa28d56
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-21 21:30:42 -0800

    updated instances.sql

--- commons/instances.sql
+++ commons/instances.sql
@@ -1,26 +1,20 @@
-INSERT INTO user (ID, username, age, gender, phoneNumber, email, password)
-VALUES (1, "dot", 18, "cis woman", "07734", "dot@example.com", "ovo!!!!!!");
+INSERT INTO user (ID, username, age, gender, phoneNumber, email, password, registered)
+VALUES (1, "dot", 18, "cis woman", "07734", "dot@example.com", "ovo!!!!!!", 1);
 
-INSERT INTO user (ID, username, age, gender, phoneNumber, email, password)
-VALUES (2, "stringbean", 18, "cis man", "55378008", "stringbean@example.com", "late");
+INSERT INTO user (ID, username, age, gender, phoneNumber, email, password, registered)
+VALUES (2, "stringbean", 18, "cis man", "55378008", "stringbean@example.com", "late", 1);
 
-INSERT INTO user (ID, username, age, gender, phoneNumber, email, password)
-VALUES (3, "vegetable", 18, "non-binary", "379009", "vegetable@example.com", "chili");
+INSERT INTO user (ID, username, age, gender, phoneNumber, email, password, registered)
+VALUES (3, "vegetable", 18, "non-binary", "379009", "vegetable@example.com", "chili", 1);
 
-INSERT INTO user (ID, username, age, gender, phoneNumber, email, password)
-VALUES (4, "potato", 18, "trans woman", "004008", "tomato@example.com", "tomato");
+INSERT INTO user (ID, username, age, gender, phoneNumber, email, password, registered)
+VALUES (4, "potato", 18, "trans woman", "004008", "tomato@example.com", "tomato", 1);
 
 INSERT INTO counsellor (ID, firstName, lastName, email, password)
 VALUES (1, "Bean", "McFluff", "beanmcfluff@example.com", "beanisapug");
 
 INSERT INTO counsellor (ID, firstName, lastName, email, password)
 VALUES (2, "Fox", "McCloud", "foxmccloud@example.com", "corneria");
-
-INSERT INTO chatroom (ID, counsellorID, userID)
-VALUES (1, 1, 1);
-
-INSERT INTO chatroom (ID, counsellorID, userID)
-VALUES (2, 1, 2);
 
 INSERT INTO session (ID, beginTime, endTime, counsellorID, userID)
 VALUES (1, "1970-01-01 00:00:01", "1980-01-01 00:00:01", 1, 1);
@@ -37,11 +31,11 @@ VALUES (4, "2003-01-01 00:00:01", "2010-01-01 00:00:01", 1, 4);
 INSERT INTO session (ID, beginTime, endTime, counsellorID, userID)
 VALUES (5, "2017-08-08 19:20:01", "2017-01-01 20:00:01", 1, 1);
 
-INSERT INTO message(ID, chatroomID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
-VALUES (1, 1, 1, "1970-01-01 00:00:01", 1, 1, "haii", 0, 1);
+INSERT INTO message(ID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
+VALUES (1, 1, "1970-01-01 00:00:01", 1, 1, "haii", 0, 1);
 
-INSERT INTO message(ID, chatroomID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
-VALUES (2, 1, 1, "1973-01-01 00:00:01", 1, 1, "i am on the web app", 0, 0);
+INSERT INTO message(ID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
+VALUES (2, 1, "1973-01-01 00:00:01", 1, 1, "i am on the web app", 0, 0);
 
-INSERT INTO message(ID, chatroomID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
-VALUES (3, 1, 1, "1979-01-01 00:00:01", 1, 1, "sup", 1, 0);
+INSERT INTO message(ID, sessionID, messageTime, counsellorID, userID, messageContent, fromCounsellor, fromTwilio)
+VALUES (3, 1, "1979-01-01 00:00:01", 1, 1, "sup", 1, 0);
