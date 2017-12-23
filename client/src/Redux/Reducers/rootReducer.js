ec439da9276195147862c793a9646ce321b33561
Stephanie Chan <stephaniechan219@gmail.com>
2017-12-23 02:42:18 -0800

    removed unused code

--- client/src/Redux/Reducers/rootReducer.js
+++ client/src/Redux/Reducers/rootReducer.js
@@ -2,14 +2,12 @@ import { combineReducers } from 'redux';
 import messages from './messagesReducer';
 import rooms from './roomsReducer';
 import activeRoom from './activeRoomReducer';
-import signInReducer from './signInReducer';
 import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   messages,
   rooms,
   activeRoom,
-  signInReducer,
   auth: authReducer
 });
 
