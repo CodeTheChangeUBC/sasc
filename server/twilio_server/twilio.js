84621b91f4c5d416164d88597cfd31dfc97a53a3
Stephanie Chan <stephaniechan219@gmail.com>
2018-01-19 01:25:21 -0800

    Removed some functions from twilio

--- server/twilio_server/twilio.js
+++ server/twilio_server/twilio.js
@@ -21,31 +21,3 @@ function createTwilioClient(twilioInfo) {
     const client = require("twilio")(accountSid, authToken);
     return client;
 }
-
-exports.getAllMessagesFromTwilio = function () {
-    const twilio = getTwilioInfo();
-    const client = createTwilioClient(twilio);
-    client.messages.each(function (message) {
-        message.body;
-        message.date_sent;
-        message.from;
-        message.to;
-    });
-};
-
-exports.getNewMessagesFromTwilio = function () {
-
-};
-
-exports.sendMessageBySMS = function (messageContent) {
-    const twilio = getTwilioInfo();
-    const client = createTwilioClient(twilio);
-    var message = {
-        to: "",
-        from: twilio.twilioPhoneNumber,
-        body: messageContent
-    };
-    client.messages.create(message).then(function (err, message) {
-    
-    });
-};
