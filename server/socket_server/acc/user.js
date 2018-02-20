4cb2a501a49999d53a74cb2f2a3a5d2947c4455f
joe-tom <joey6978@gmail.com>
2018-02-19 19:06:17 -0800

    Begun architecting out the OO design

--- server/socket_server/acc/user.js
+++ server/socket_server/acc/user.js
@@ -1,70 +1,22 @@
 
-
-//var Patient = require('./patient')
-
-
-//class User {
-
-  /**
-   * Creates a user based on the socket.data data.
-   * @param  {Socket} socket - The Socket.io socket.
-   * @return {User}          - Returns a user either a counselor or patient.
-   */
-  /*static create (socket) {
-    var user
-
-    switch (socket.data.type) {
-      case 'COUNSELOR':
-        user = new Counselor(socket)
-      break
-      case 'PATIENT':
-        user = new Patient(socket)
-      break
-    }
-
-    // Add it to the global map.
-    this.users[socket.id] = user
-
-    return user
-  }
-}
-
-User.users = {}
-
-
-
-module.exports = User*/
-
-var UserMap = {}
-
-UserMap['Counselor'] = {}
-UserMap['Student'] = {}
+import Counsellor from './counsellor'
+import Patient from './patient'
 
 
 class User {
-  constructor (socket, type, obj) {
-    UserMap[type][socket.id] = obj
-  }
+    constructor (socket, type, obj) {
+        this[type] = obj
+    }
 
-  /**
-   * Searches the CounselorMap for a user
-   * @param  {Student} student - A student object
-   * @return {Counselor}
-   */
-  static find (student) {
-    // This could be optimized.
-    // Essentially, it creates an array of keys from the UserMap.Counselor Object
-    // then sorts the keys based on the smallest conversation array length
-    // then returns the smallest one.
-    
-    var c_id = Object
-    .keys(UserMap.Counselor)
-    .sort((a,b) => {
-      UserMap.Counselor[a].convos.length - UserMap.Counselor[b].convos.length
-    })[0]
-
-    return UserMap.Counselor[c_id]
-  }
+    /**
+     * Attempts to match the patient with a counsellor
+     */
+    static match () {
+    }
 }
 
-module.exports = User
+
+User.Counsellors = {}
+User.Patients = {}
+
+export default User
--- server/socket_server/socket.js
+++ server/socket_server/socket.js
@@ -6,51 +6,36 @@
 // 
 // Communication between sockets is done in User objects themselves.
 
-var Counsellor = require('./acc/counsellor')
-var Student = require('./acc/student')
-//var users = require('./acc/users') 
+var config = require('../../config')
+var users = require('./acc/users') 
 
-var util = require('./../../config')
 var jwt = require('jsonwebtoken')
 
 module.exports = io => {
-
-  // Check and accept/reject connection based on token.
-  io.use((socket, next) => {
-    var token = socket.request.query.t
-    var secret = util.secret
-
-    // Verify and decode the token.
-    jwt.verify(token, secret, (error, data) => {
-      if (error) {
-        next(error)
-      } else {
-        socket.data = data
-        next()
-      }
-    })
-  })
-
-
-  // Create a user for this socket.
-  // Set up listeners for this socket.
-  io.on('connection', socket => {
-    switch (socket.payload.type) {
-      case 'COUNSELLOR':
-        console.log('Counsellor connected!')
-        new Counsellor(socket)
-      break
-      case 'STUDENT':
-        console.log('Student connected!')
-        new Student(socket)
-      break
-    }
     
-    //var user = users.create(socket)
+    // Check and accept/reject connection based on token.
+    io.use((socket, next) => {
+        var token = socket.request.query.t
+        var secret = util.secret
 
-    // Sends the msg, to the appropriate user.
-    // i.e.
-    //  Counsellor <-> Student
-    //socket.on('msg', data => (user.relay(data))
-  })
+        // Verify and decode the token.
+        jwt.verify(token, secret, (error, data) => {
+            if (error) {
+                next(error)
+            } else {
+                socket.payload = data
+                next()
+            }
+        })
+    })
+
+
+    // Create a user for this socket.
+    // Set up listeners for this socket.
+    io.on('connection', socket => {
+        var req = socket.request
+        console.info(`Connected: IP ${req.connection.remoteAddress}`)
+
+        new User(socket)
+    })
 }
