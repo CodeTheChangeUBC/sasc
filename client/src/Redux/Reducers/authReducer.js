030e3d9f985341e104959c4603205feb6f76c250
Stephanie Chan <stephaniechan219@gmail.com>
2018-02-23 20:23:42 -0800

    Modify auth reducer

--- client/src/Redux/Reducers/authReducer.js
+++ client/src/Redux/Reducers/authReducer.js
@@ -1,8 +1,7 @@
 import {
     AUTH_USER,
     AUTH_COUNSELLOR,
-    UNAUTH_USER,
-    UNAUTH_COUNSELLOR,
+    UNAUTH,
     AUTH_ERROR,
     REMOVE_ERROR
 } from './../Types/authTypes';
@@ -11,17 +10,15 @@ import initialState from './initialState';
 export default function(state = initialState.auth, action) {
     switch(action.type) {
         case AUTH_USER:
-            return {...state, error: '', authenticated: true, authenticatedCounsellor: false, role: "user"};
+            return {...state, error: "", role: "user", success: "Successfully logged in."};
         case AUTH_COUNSELLOR:
-            return {...state, error: '', authenticated: false, authenticatedCounsellor: true, role: "counsellor"};
-        case UNAUTH_USER:
-            return {...state, authenticated: false, role: ""};
-        case UNAUTH_COUNSELLOR:
-            return {...state, authenticatedCounsellor: false, role: ""};
+            return {...state, error: "", role: "counsellor", success: "Successfully logged in."};
+        case UNAUTH:
+            return {...state, role: "", success: "Successfully logged out."};
         case AUTH_ERROR:
             return {...state, error: action.payload};
         case REMOVE_ERROR:
-            return {...state, error: '', success: ''};
+            return {...state, error: "", success: ""};
     }
 
     return state;
