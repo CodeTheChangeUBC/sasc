c577e0c1a4a8172454aa16281826f02c942dd631
Stephanie Chan <stephaniechan219@gmail.com>
2018-02-24 17:35:01 -0800

    Update twilio backend continued

--- server/twilio_server/twilio.js
+++ server/twilio_server/twilio.js
@@ -1,23 +1,9 @@
 const twilioModel = require("../models/twilio");
 
-function getTwilioInfo() {
-    twilioModel.list(function (err, results) {
-        if (err) {
-            return res.status(422).send({error: "Failed to retrieve twilio account information."});
-        }
-
-        if (results.length === 0) {
-            return res.status(422).send({error: "There is no existing twilio account information yet."});
-        }
-
-        // We are expecting only one result
-        return results[0];
-    });
-}
-
-function createTwilioClient(twilioInfo) {
-    const accountSid = twilioInfo.accountSid;
-    const authToken = twilioInfo.authToken;
+function createTwilioClient() {
+    const twilio = twilioModel.getSMSFields();
+    const accountSid = twilio.accountSid;
+    const authToken = twilio.authToken;
     const client = require("twilio")(accountSid, authToken);
     return client;
 }
